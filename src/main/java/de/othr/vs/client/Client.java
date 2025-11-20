5576b1215ee60139348d38104f7ca0524d35b93c
Noah Jutz <noah@noahjutz.com>
2025-11-20 12:18:55 +0100

    Group client calls into functions, implement stream client

--- src/main/java/de/othr/vs/client/Client.java
+++ src/main/java/de/othr/vs/client/Client.java
@@ -1,5 +1,6 @@
 package de.othr.vs.client;
 
+import io.grpc.Channel;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
@@ -7,15 +8,34 @@ import java.util.concurrent.TimeUnit;
 import your.pkg.Bewertung;
 import your.pkg.Messwert;
 import your.pkg.SimpleActionServiceGrpc;
+import your.pkg.StreamActionServiceGrpc;
 
 public class Client {
+  public static Messwert sampleMesswert =
+      Messwert.newBuilder().setSensor("Wasserstand").setValue(9.4).build();
 
   public static void main(String[] args) throws InterruptedException {
     ManagedChannel channel =
         ManagedChannelBuilder.forAddress("localhost", 1234).usePlaintext().build();
 
-    var sampleMesswert = Messwert.newBuilder().setSensor("Wasserstand").setValue(9.4).build();
+    System.out.println("simpleBlocking");
+    simpleBlocking(channel);
+    System.out.println("simpleCallback");
+    simpleCallback(channel);
+    System.out.println("streamCallback");
+    streamCallback(channel);
 
+    channel.awaitTermination(30L, TimeUnit.SECONDS);
+  }
+
+  private static void simpleBlocking(Channel channel) {
+    var blockingStub = SimpleActionServiceGrpc.newBlockingStub(channel);
+
+    var res = blockingStub.getRequiredAction(sampleMesswert);
+    System.out.println(res);
+  }
+
+  private static void simpleCallback(Channel channel) {
     var stub = SimpleActionServiceGrpc.newStub(channel);
 
     stub.getRequiredAction(
@@ -32,11 +52,28 @@ public class Client {
             System.out.println(b);
           }
         });
+  }
 
-    var blockingStub = SimpleActionServiceGrpc.newBlockingStub(channel);
+  private static void streamCallback(Channel channel) {
+      var streamStub = StreamActionServiceGrpc.newStub(channel);
 
-    var res = blockingStub.getRequiredAction(sampleMesswert);
-    System.out.println(res);
-    channel.awaitTermination(30L, TimeUnit.SECONDS);
+      var handle =
+          streamStub.streamRequiredAction(
+              new StreamObserver<Bewertung>() {
+                  @Override
+                  public void onNext(Bewertung bewertung) {
+                      System.out.println(bewertung);
+                  }
+
+                  @Override
+                  public void onError(Throwable throwable) {}
+
+                  @Override
+                  public void onCompleted() {}
+              });
+
+      handle.onNext(sampleMesswert);
+      handle.onNext(sampleMesswert);
+      handle.onNext(sampleMesswert);
   }
 }
