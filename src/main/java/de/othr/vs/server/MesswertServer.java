2d61b40c6d3f95acf2d264299e8fa26c474e0b24
Noah Jutz <noah@noahjutz.com>
2025-11-20 11:43:36 +0100

    Implement basic server and simple service

--- src/main/java/de/othr/vs/server/MesswertServer.java
+++ src/main/java/de/othr/vs/server/MesswertServer.java
@@ -1,21 +1,51 @@
 package de.othr.vs.server;
 
+import com.google.protobuf.Timestamp;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import your.pkg.Bewertung;
+import your.pkg.Messwert;
+import your.pkg.SimpleActionServiceGrpc.SimpleActionServiceImplBase;
+
+class SimpleActionServiceImpl extends SimpleActionServiceImplBase {
+
+    @Override
+    public void getRequiredAction(
+        Messwert request,
+        StreamObserver<Bewertung> responseObserver
+    ) {
+        responseObserver.onNext(
+            Bewertung.newBuilder()
+                .setAction("Do something")
+                .setTime(
+                    Timestamp.newBuilder()
+                        .setSeconds(
+                            TimeUnit.MILLISECONDS.toSeconds(
+                                System.currentTimeMillis()
+                            )
+                        )
+                        .build()
+                )
+                .build()
+        );
+        responseObserver.onCompleted();
+    }
+}
+
 public class MesswertServer {
 
-    public static final String SERVER_GRPC_HOST = "localhost";
-    public static final int SERVER_GRPC_PORT    = 1234;
-
-    public static void main(String[] args) {
-        /*
-        Server server = ServerBuilder.forPort(SERVER_GRPC_PORT)
-                .addService(new MyServiceImpl())
-                .build();
+    public static void main(String[] args)
+        throws IOException, InterruptedException {
+        var server = ServerBuilder.forPort(1234)
+            .addService(new SimpleActionServiceImpl())
+            .build();
 
         server.start();
 
         System.out.println("Server running...");
 
         server.awaitTermination();
-        */
     }
 }
