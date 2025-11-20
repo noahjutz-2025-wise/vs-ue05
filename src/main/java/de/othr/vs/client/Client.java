da228f3e262649d2d0939ad998eacf2f649fa5d3
Noah Jutz <noah@noahjutz.com>
2025-11-20 11:14:23 +0100

    Call service from client

--- src/main/java/de/othr/vs/client/Client.java
+++ src/main/java/de/othr/vs/client/Client.java
@@ -3,26 +3,40 @@ package de.othr.vs.client;
 import de.othr.vs.server.MesswertServer;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-
+import io.grpc.stub.StreamObserver;
 import java.util.concurrent.TimeUnit;
+import your.pkg.Bewertung;
+import your.pkg.Messwert;
+import your.pkg.SimpleActionServiceGrpc;
 
 public class Client {
 
     public static void main(String[] args) throws InterruptedException {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(
+            MesswertServer.SERVER_GRPC_HOST,
+            MesswertServer.SERVER_GRPC_PORT
+        )
+            .usePlaintext()
+            .build();
 
-        ManagedChannel channel = ManagedChannelBuilder
-                .forAddress(MesswertServer.SERVER_GRPC_HOST,
-                        MesswertServer.SERVER_GRPC_PORT)
-                .usePlaintext()
-                .build();
+        var stub = SimpleActionServiceGrpc.newStub(channel);
 
-        // Stub generieren (je nach Anwendungsfall stub, blocking stub oder future stub)
+        stub.getRequiredAction(
+            Messwert.newBuilder()
+                .setSensor("Wasserstand")
+                .setValue(9.4)
+                .build(),
+            new StreamObserver<>() {
+                @Override
+                public void onCompleted() {}
 
-        // Messages generieren und Service aufrufen
+                @Override
+                public void onError(Throwable arg0) {}
 
-
-        // Client noch nicht beenden, Callbacks vom Server (via StreamObserver::onNext, ...)
-        // werden sonst nicht mehr empfangen
+                @Override
+                public void onNext(Bewertung arg0) {}
+            }
+        );
         channel.awaitTermination(30L, TimeUnit.SECONDS);
     }
 }
